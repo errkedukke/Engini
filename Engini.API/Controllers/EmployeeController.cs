3f76948c305bd204f6615895e918b1c363579b95
Guchikata <38571128+errkedukke@users.noreply.github.com>
2025-10-30 22:14:41 +0400

    Engini.API: Changes in controller, addedd logger and prepared for future DI service implementation.

--- Engini.API/Controllers/EmployeeController.cs
+++ Engini.API/Controllers/EmployeeController.cs
@@ -1,3 +1,4 @@
+using Engini.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Engini.API.Controllers;
@@ -6,12 +7,33 @@ namespace Engini.API.Controllers;
 [Route("[controller]")]
 public sealed class EmployeeController : ControllerBase
 {
-    [HttpGet("{id:int}")]
-    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<string> Get(int id, CancellationToken cancellationToken)
+    private readonly ILogger<EmployeeController> _logger;
+
+    public EmployeeController(ILogger<EmployeeController> logger)
     {
-        await Task.Delay(id, cancellationToken);
-        throw new NotImplementedException();
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Returns a specific employee with all its subordinates (recursively).
+    /// </summary>
+    /// <param name="id">Root employee ID</param>
+    /// <param name="cancellationToken">Request cancellation token</param>
+    [HttpGet("{id:int}")]
+    [ProducesResponseType(typeof(Employee), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Get(int id, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Fetching employee hierarchy for ID {Id}", id);
+
+        var employee = new Employee(); //await _service.ExecuteAsync(id, cancellationToken);
+
+        if (employee is null)
+        {
+            _logger.LogWarning("Employee with ID {Id} not found", id);
+            return NotFound(new { message = $"Employee with ID {id} not found." });
+        }
+
+        return Ok(employee);
     }
 }
